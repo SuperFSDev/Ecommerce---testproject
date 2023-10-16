c05fa7e5591cc7a6613c907d0cb4275d3e429c91
superfsdev <krishi119@outlook.com>
2023-10-15 22:59:57 -0700

    complete frontend

---
+++ client/src/components/Table.jsx
@@ -0,0 +1,89 @@
+import { useState, useMemo } from "react";
+import Pagination from "./Pagenation";
+import { CheckBox } from "@material-ui/icons";
+
+let PageSize = 10;
+
+export function Table({ products }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [items, setItems] = useState(products);
+  const [isAsc, setIsAsc] = useState(true);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return items.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, items]);
+
+  const sortByTitle = (e) => {
+    const products = items;
+    if (isAsc) {
+      e.target.setAttribute("aria-sort", "descending");
+      products.sort((a, b) => {
+        return a.title.localeCompare(b.title);
+      });
+    } else {
+      e.target.setAttribute("aria-sort", "ascending");
+      products.sort((a, b) => {
+        return b.title.localeCompare(a.title);
+      });
+    }
+    setIsAsc(!isAsc);
+    setItems([...products]);
+  };
+
+  const search = (e) => {
+    const searchQuery = e.target.value;
+    const searchedResult = products.filter((product) =>
+      product.title.includes(searchQuery)
+    );
+    setItems(searchedResult);
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          id="myInput"
+          onKeyUp={(e) => search(e)}
+          placeholder="Search for titles..."
+          title="Type in a title"
+        />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th aria-sort="ascending" onClick={(e) => sortByTitle(e)}>
+              Title
+              <span aria-hidden="true"></span>
+            </th>
+            <th>Description</th>
+            <th>Color</th>
+            <th>Size</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map((record) => (
+            <tr key={record.id}>
+              <td>{record.title}</td>
+              <td>{record.desc}</td>
+              <td>{record.color}</td>
+              <td>{record.size}</td>
+              <td>{record.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={products.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
+    </>
+  );
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { Table } from "../components/Table";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "../styles/style.css";
+
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(`http://localhost:4000/api/products`);
+        console.log("Products");
+        console.log(res.data);
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+    setLoading(false);
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  if (products.length === 0) return <p>No records found</p>;
+
+  return (
+    <div className="container">
+      <Table products={products} />
+    </div>
+  );
+};
+
+export default Home;
